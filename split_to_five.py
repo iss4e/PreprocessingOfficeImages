5aee2fa2e8edbe40c8a74391349b544e07164bd7
sashaDoubov <doubovs@gmail.com>
2018-03-19 22:42:09 -0400

    Add changes to split to five

--- split_to_five.py
+++ split_to_five.py
@@ -3,11 +3,13 @@ from PIL import Image, ImageDraw
 from collections import defaultdict
 import sys
 from shutil import copyfile
+from tqdm import tqdm
 import random
 
 
 def split_image(image_name):
-    img_path = os.path.join(processed_path, image_name)
+    
+    img_path = os.path.join(processed_path, image_name + '.jpg')
     img = Image.open(img_path)
     
     width, height = img.size
@@ -21,8 +23,13 @@ def split_image(image_name):
     
     center_factor = 104
 
-    draw.rectangle(((width/4 - center_factor, height/4), (3*width/4 - center_factor, 3*height/4)), outline='red')
-    img.show()
+    
+    center = img.crop((width/4 - center_factor, height/4, 3*width/4 - center_factor, 3*height/4 - 1))
+    
+    split_images = [top_left, top_right, bottom_left, bottom_right, center] # order is important due to indexing
+    
+    return split_images
+   
      
 
 processed_path = sys.argv[1]
@@ -39,7 +46,7 @@ for i, filename in enumerate(listOfFiles):
     if ext == 'txt':
         with open(os.path.join(processed_path, filename), 'r') as f:
             category = f.read()
-            category_to_image[category].append("{}.jpg".format(name))
+            category_to_image[category].append(name)
             
 right_bottom_images = category_to_image["00010"]
 random.shuffle(right_bottom_images)
@@ -49,13 +56,22 @@ for category, image_list in category_to_image.iteritems():
     #print "{}  has {} elements".format(category, len(image_list))
     assert len(category) == 5
     
-    split_image(image_list[0])
-    input_data  = raw_input("ask_for_input?? ")
-    for index in category:
-        if index == '1':
-            pass
-        elif index == '0':
-            pass
+    for image in tqdm(image_list):
+        split_images = split_image(image)
+        
+        for i, binary_value  in enumerate(category):
+        
+            split_name = "{}&{}".format(image, i)
+             
+            split_images[i].save(os.path.join(output_path, split_name + '.jpg'))
+            
+            txt_path = os.path.join(output_path, split_name + '.txt')
+            
+            with open(txt_path, 'w+') as txt_file:
+                txt_file.write(binary_value)
+            
+            
+    
         
        
        
