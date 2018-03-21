79bc98429816aebec5b8259dc5e1e5663e3292de
sashaDoubov <doubovs@gmail.com>
2018-03-20 21:45:55 -0400

    adding files

---
+++ random_brightness_contrast.py
@@ -0,0 +1,55 @@
+import os
+from PIL import Image, ImageEnhance
+import sys
+import ntpath
+from tqdm import tqdm
+from shutil import copyfile
+
+import numpy
+
+processed_path = sys.argv[1]
+
+listOfFiles = os.listdir(processed_path)
+lenFiles = len(listOfFiles)
+
+for filename in tqdm(listOfFiles):
+    name, exten = os.path.splitext(filename)
+    
+    if  'txt' in exten:
+        continue
+    
+    file = os.path.join(processed_path, filename)
+    
+    img = Image.open(file) 
+    
+    transformed_images = [img for i in range(5)]
+    
+    
+    for i, tr_image in enumerate(transformed_images):
+    
+        contrast_value = numpy.random.normal(1,0.025)
+        brightness_value = numpy.random.normal(1,0.05)
+        
+        #contrast_value = 1
+        #brightness_value = 1-.22
+        
+        b_enhancer = ImageEnhance.Brightness(tr_image)
+        
+        tr_image = b_enhancer.enhance(brightness_value)
+        
+        c_enhancer  = ImageEnhance.Contrast(tr_image)
+        tr_image = c_enhancer.enhance(contrast_value)
+        
+        transformed_name = "{}__{}".format(name, i)
+        tr_image.save(os.path.join(processed_path, transformed_name +'.jpg'))
+        
+        text_filename = transformed_name + '.txt'
+        old_text_filename = name + '.txt'
+    
+        new_text_path = os.path.join(processed_path, text_filename)
+        old_text_path = os.path.join(processed_path, old_text_filename)
+        
+        copyfile(old_text_path, new_text_path)
+
+
+
--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image, ImageDraw
 from collections import defaultdict
 import sys
+import random
 
 processed_path = sys.argv[1]
 
@@ -19,26 +20,46 @@ for i, filename in enumerate(listOfFiles):
             category_to_image[category].append("{}.jpg".format(name))
             
 
+right_bottom_images = category_to_image["00010"]
+random.shuffle(right_bottom_images)
+category_to_image["00010"] = right_bottom_images[:150]
+
 num_per_index = defaultdict(list)
 for category, image_list in category_to_image.iteritems():
     print "{}  has {} elements".format(category, len(image_list))
     
     for i in range(5):
         if category[i] == '1':
-            num_per_index[i].extend(image_list)
-           
+            num_per_index[i].extend(image_list)      
+sum_i  =0   
 print "-------------------------------------------------------"
 for index, number_of_imgs in num_per_index.iteritems():
-    print "Index {} has {} images".format(index, len(number_of_imgs))
+    print "Index {} has {}/{} images".format(index, len(number_of_imgs), lenFiles/2)
+    sum_i += len(number_of_imgs)
+print "sum ", sum_i
+
+num_per_index = defaultdict(list)
+for category, image_list in category_to_image.iteritems():
     
-for filename in num_per_index[1]:
-    file = os.path.join(processed_path, filename)
-    print os.path.isfile(file)
+    for i in range(5):
+        if category[i] == '0':
+            num_per_index[i].extend(image_list)
+            
+print "-------------------------------------------------------"
+sum_i = 0
+for index, number_of_imgs in num_per_index.iteritems():
+    print "Index {} has {}/{} images".format(index, len(number_of_imgs), lenFiles/2)
+    sum_i += len(number_of_imgs)
+print "sum ", sum_i
     
-    img = Image.open(file) 
-    img.show()
-    input = raw_input("opening file: {} ".format(filename))
+# for filename in num_per_index[1]:
+    # file = os.path.join(processed_path, filename)
+    # print os.path.isfile(file)
     
-    img.close()
+    # img = Image.open(file) 
+    # img.show()
+    # input = raw_input("opening file: {} ".format(filename))
+    
+    # img.close()
             
     
