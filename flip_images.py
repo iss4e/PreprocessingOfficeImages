6ae5b1622d1e2dd25e3a507073b2f0478556814a
sashaDoubov <doubovs@gmail.com>
2018-05-05 17:33:34 -0400

    Add doc

--- flip_images.py
+++ flip_images.py
@@ -5,9 +5,15 @@ import ntpath
 from tqdm import tqdm
 from shutil import copyfile
 
-processed_path = sys.argv[1]
+"""
+Flips image files horizontally, in-place
+To use: python flip_images.py <image folder path>
 
-listOfFiles = os.listdir(processed_path)
+"""
+
+image_folder_path = sys.argv[1]
+
+listOfFiles = os.listdir(image_folder_path)
 lenFiles = len(listOfFiles)
 
 for filename in tqdm(listOfFiles):
@@ -16,19 +22,19 @@ for filename in tqdm(listOfFiles):
     if  'txt' in exten:
         continue
     
-    file = os.path.join(processed_path, filename)
+    file = os.path.join(image_folder_path, filename)
     
     img = Image.open(file) 
     img = ImageOps.mirror(img)
     
     mirror_name = "{}_f".format(filename.split('.')[0])
-    img.save(os.path.join(processed_path, mirror_name + '.jpg'))
+    img.save(os.path.join(image_folder_path, mirror_name + '.jpg'))
     
     text_filename = mirror_name + '.txt'
     old_text_filename = filename.split('.')[0] + '.txt'
     
-    new_text_path = os.path.join(processed_path, text_filename)
-    old_text_path = os.path.join(processed_path, old_text_filename)
+    new_text_path = os.path.join(image_folder_path, text_filename)
+    old_text_path = os.path.join(image_folder_path, old_text_filename)
     
     copyfile(old_text_path, new_text_path)
     
